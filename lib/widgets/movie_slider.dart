7a9b11ce40961b9974c686d1830373a08c2e7fc8
reidaza <randres2611@gamil.com>
2022-02-22 10:34:51 -0500

    Version Hero, para animacion

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -64,7 +64,7 @@ class _MovieSliderState extends State<MovieSlider> {
               controller: scrollController,
               scrollDirection: Axis.horizontal,
               itemCount: widget.movies.length,
-              itemBuilder: ( _ , int index) =>  _MoviePoster( widget.movies[index] )
+              itemBuilder: ( _ , int index) =>  _MoviePoster( widget.movies[index], '${widget.title}-${widget.movies[index].id}' )
             ),
           ),
         ],
@@ -77,10 +77,12 @@ class _MovieSliderState extends State<MovieSlider> {
 
 class _MoviePoster extends StatelessWidget {
   final Movie movie;
-
-  const _MoviePoster(this.movie,);
+  final String heroId;
+  const _MoviePoster(this.movie, this.heroId,);
   @override
   Widget build(BuildContext context) {
+
+    movie.heroId = heroId;
     return Container(
       width: 130,
       height: 190,
@@ -89,15 +91,18 @@ class _MoviePoster extends StatelessWidget {
         children: [
           GestureDetector(
             onTap: () => Navigator.pushNamed(context, 'details', arguments: movie),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'), 
-                image: NetworkImage(movie.fullPosterImg),
-                width: 130,
-                height: 190,
-                fit: BoxFit.cover,
-                ),
+            child: Hero(
+              tag: movie.heroId!,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/no-image.jpg'), 
+                  image: NetworkImage(movie.fullPosterImg),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                  ),
+              ),
             ),
           ),
           const SizedBox(height: 5,),  
